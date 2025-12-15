07bf73321d69c6ce633004c92cbf19bd0b6f3e77
ilham269 <ilhm282@gmail.com>
2025-12-15 11:45:38 +0700

    well

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/sapa/{nama}', function ($nama) {
+
+    return "Hallo nama saya adalah $nama.";
+});
+Route::get('/hitung/{angka1}/{angka2}', function ($angka1, $angka2) {
+
+    $hasil = $angka1 + $angka2;
+    return "Hasil dari $angka1 + $angka2 adalah $hasil.";
+});
+Route::get('/kategori/{nama?}', function ($nama="semua") {
+
+    return "Anda manampilkan kategori $nama.";
+});
+
